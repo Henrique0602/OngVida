a759f25cde003c90bd2e5dcf6015ffe85cf943ba
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-30 13:41:45 -0300

    REvisao

---
+++ cadvida.js
@@ -0,0 +1,71 @@
+//requisitando os modulos
+
+const mongoose=require("mongoose");
+const express = require("express");
+const bodyparser = require("body-parser");
+
+// configurando o express para o postamn e para usar a pagina
+
+const app = express();
+app.use(bodyparser.json());
+const port =3000;
+
+// configurando o banco de dados
+mongoose.connect("mongodb://127.0.0.1:27017/dbvida",
+{
+    useNewUrlParser:true,
+    useUnifiedTopology : true,
+}
+)
+
+// criando a model a do seu projeto
+
+const UsuarioSchema = new  mongoose.Schema({
+
+    nome : {type : String},
+    email : {type : String , required : true},
+    endereco : {type : String},
+    numero : {type : Number},
+    cep : {type : String, required: true },
+    nascimento : {type : Date, required : true}
+});
+
+const Usuario = mongoose.model("Usuario",UsuarioSchema);
+
+//configuração dos roteamentos 
+//cadastroUsuario
+
+app.post("/cadastrousuario" , async(req , res)=>{
+    const nome = req.body.nome;
+    const email = req.body.email;
+    const endereco = req.body.endereco;
+    const numero = req.body.numero;
+    const cep = req.body.cep;
+    const nascimento = req.body.nascimento;
+
+
+const usuario = new Usuario({
+    nome : nome,
+    email : email,
+    endereco : endereco,
+    numero : numero,
+    cep : cep,
+    nascimento : nascimento
+})
+
+try{
+    const newUsuario = await usuario.save();
+    res.json({error : null, msg : "Cadastro ok ", UsuarioId : newUsuario._id});
+}catch (error) {}
+
+});
+
+app.get("/" , async(req, res)=>{
+    res.sendFile(__dirname + "/index.html")
+});
+
+app.listen(port, ()=>{
+    console.log(`Servidor rodando na porta ${port}`)
+})
+
+
